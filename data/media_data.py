f33d9c632a484d485ac50962d7f5109f74e839dc
Pavan Kumar <pavankumarpalla3@gmali.com>
2026-04-21 16:04:55 +0530

    update all files

--- data/media_data.py
+++ data/media_data.py
@@ -1,7 +1,7 @@
 IMAGE_PATHS = [
-    "/static/media/images/1.jpg",
-    "/static/media/images/2.jpg",
-    "/static/media/images/3.jpg"
+    "/static/media/images/1.png",
+    "/static/media/images/2.png",
+    "/static/media/images/3.png"
 ]
 
 VIDEO_PATH = "/static/media/videos/college.mp4"
